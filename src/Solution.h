c6fe10e35f3c950b19f90bd57fe0ca670739811a
Rafael Santin <rasantin@gmail.com>
2026-01-24 00:14:04 -0300

    Style: remove comment from unordered_map include

--- src/Solution.h
+++ src/Solution.h
@@ -22,7 +22,7 @@
 #include <list>
 #include <tuple>
 #include <queue>
-#include <unordered_map>  // Added
+#include <unordered_map>
 #include <utility>  // std::pair
 
 	// Functor para permitir unordered_map com std::pair<int, int> como chave
